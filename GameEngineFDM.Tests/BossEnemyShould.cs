8dd253541a63add92e9f88f75a533b3a24612878
Missy Maloney <missymaloney1@gmail.com>
2022-02-23 09:12:29 -0500

    first commit

---
+++ GameEngineFDM.Tests/BossEnemyShould.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace GameEngineFDM.Tests
+{
+    public class BossEnemyShould
+    {
+
+        [Fact]
+        public void HaveCorrectPower()
+        {
+            BossEnemy sut = new BossEnemy();
+
+            Assert.Equal(166.667, sut.SpecialAttackPower, 3);
+        }
+
+
+    }
+}
---
+++ GameEngineFDM/NormalEnemy.cs
@@ -0,0 +1,8 @@
+﻿namespace GameEngineFDM
+{
+    public class NormalEnemy : Enemy
+    {
+        public override double TotalSpecialPower => 100;
+        public override double SpecialPowerUses => 2;
+    }
+}
